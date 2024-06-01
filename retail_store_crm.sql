73554606eca5594112ff6ef23b02fc83ce80f0cb
Jahidul Islam <jahidulzaid@gmail.com>
2024-06-01 06:15:15 +0600

    updated db schema

--- retail_store_crm.sql
+++ retail_store_crm.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 10, 2024 at 07:00 PM
+-- Generation Time: Jun 01, 2024 at 01:54 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -43,9 +43,107 @@ CREATE TABLE `appuser` (
 --
 
 INSERT INTO `appuser` (`appuser_pk`, `userRole`, `name`, `mobileNumber`, `email`, `password`, `address`, `status`) VALUES
-(1, 'Admin', 'Jahidul Islam', '1234567890', 'jahidul@gmail.com', 'password123', '123 Kanchan, Vulta, Rupgonj', 'Active'),
+(1, 'SuperAdmin', 'Jahidul Islam', '1234567890', 'jahidul@gmail.com', 'pass123', '123 Kanchan, Vulta, Rupgonj', 'Active'),
 (2, 'Admin', 'test1', '1', '1@gmail.com', '123', 'Test address1', 'Active'),
-(3, 'Admin', 'User', '12333', '123@gmail.com', '123', '123 address Updated', 'Active');
+(3, 'Admin', 'User', '12333', '123@gmail.com', '123', '123 address Updated', 'Active'),
+(4, 'Admin', 'Add', '1234', 'aa@gmail.com', '1111', 'Address', 'Active'),
+(5, 'admin', 'Rafiq Hossain', '01798765432', 'rafiq.hossain@example.com', 'securepassword123', '123 Bashundhara, Dhaka, Bangladesh', 'active'),
+(6, 'user', 'Laila Rahman', '01898765432', 'laila.rahman@example.com', 'anothersecurepassword', '456 Uttara, Dhaka, Bangladesh', 'inactive'),
+(7, 'moderator', 'Kamal Uddin', '01998765432', 'kamal.uddin@example.com', 'yetanotherpassword', '789 Mohammadpur, Dhaka, Bangladesh', 'active'),
+(8, 'guest', 'Nazia Hasan', '01698765432', 'nazia.hasan@example.com', 'someotherpassword', '321 Badda, Dhaka, Bangladesh', 'pending'),
+(9, 'Admin', 'sfdg', 'adsf', 'dfg', 'dfg', 'asdfg', 'Active');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `category_sl` int(11) NOT NULL,
+  `NAME` varchar(200) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`category_sl`, `NAME`) VALUES
+(1, 'b'),
+(2, 'a'),
+(3, 'a'),
+(4, 'a'),
+(5, 'aa');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer`
+--
+
+CREATE TABLE `customer` (
+  `customer_sl` int(11) NOT NULL,
+  `name` varchar(200) DEFAULT NULL,
+  `mobileNumber` varchar(50) DEFAULT NULL,
+  `email` varchar(200) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `customer`
+--
+
+INSERT INTO `customer` (`customer_sl`, `name`, `mobileNumber`, `email`) VALUES
+(1, 'Tanvir', '01881223349', 'tanvirr@gmail.com');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orderdetail`
+--
+
+CREATE TABLE `orderdetail` (
+  `order_sl` int(11) NOT NULL,
+  `orderId` varchar(200) DEFAULT NULL,
+  `customer_fk` int(11) DEFAULT NULL,
+  `orderDate` varchar(200) DEFAULT NULL,
+  `totalPaid` float DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `orderdetail`
+--
+
+INSERT INTO `orderdetail` (`order_sl`, `orderId`, `customer_fk`, `orderDate`, `totalPaid`) VALUES
+(1, 'Bill-3739616010100', 0, '01-06-2024', 160),
+(2, 'Bill-3898584133300', 0, '01-06-2024', 160),
+(3, 'Bill-4211515792899', 0, '01-06-2024', 160),
+(4, 'Bill-4355305922400', 0, '01-06-2024', 320),
+(5, 'Bill-4693846320000', 0, '01-06-2024', 25600),
+(6, 'Bill-5427841146000', 0, '01-06-2024', 640),
+(7, 'Bill-8280566739000', 0, '01-06-2024', 100);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+
+CREATE TABLE `product` (
+  `product_sl` int(11) NOT NULL,
+  `name` varchar(200) DEFAULT NULL,
+  `quantity` int(11) DEFAULT NULL,
+  `price` float DEFAULT NULL,
+  `description` varchar(500) DEFAULT NULL,
+  `category_fk` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `product`
+--
+
+INSERT INTO `product` (`product_sl`, `name`, `quantity`, `price`, `description`, `category_fk`) VALUES
+(1, 'Water', 100, 10, 'Fresh Water from Meghna Group', 1),
+(2, 'Milk', 163, 40, 'Agro Milk', 2);
 
 --
 -- Indexes for dumped tables
@@ -58,6 +156,30 @@ ALTER TABLE `appuser`
   ADD PRIMARY KEY (`appuser_pk`);
 
 --
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`category_sl`);
+
+--
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`customer_sl`);
+
+--
+-- Indexes for table `orderdetail`
+--
+ALTER TABLE `orderdetail`
+  ADD PRIMARY KEY (`order_sl`);
+
+--
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`product_sl`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -65,7 +187,31 @@ ALTER TABLE `appuser`
 -- AUTO_INCREMENT for table `appuser`
 --
 ALTER TABLE `appuser`
-  MODIFY `appuser_pk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `appuser_pk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `category_sl` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `customer_sl` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `orderdetail`
+--
+ALTER TABLE `orderdetail`
+  MODIFY `order_sl` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `product`
+--
+ALTER TABLE `product`
+  MODIFY `product_sl` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
